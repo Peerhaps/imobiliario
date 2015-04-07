b8df5b29ee29e56776acf555fa2bf89b42f36388
guilherme.guimaraes <guilherme.guimaraes@vdi-unijava-003.intranet.ciss>
2015-04-07 13:41:19 -0300

    Teste

--- Imobiliaria/br/com/uniciss/imobiliaria/Adminstradador.java
+++ Imobiliaria/br/com/uniciss/imobiliaria/Adminstradador.java
@@ -1,7 +1,6 @@
 package br.com.uniciss.imobiliaria;
 public class Adminstradador extends Autenticavel {
 	public void cadastrarSecretarios(Secretario secretario) {
-	
 	}
 	
 	public void cadastrarCorretor(Corretor corretor) {
