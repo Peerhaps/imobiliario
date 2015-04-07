cb3042cb7cdbc61c896e1e41bd0eaf962671d6fd
gabriel.paula <gabriel.paula@vdi-unijava-002.intranet.ciss>
2015-04-07 17:00:38 -0300

    pROGRAMA

--- Imobiliaria/br/com/uniciss/imobiliaria/Programa.java
+++ Imobiliaria/br/com/uniciss/imobiliaria/Programa.java
@@ -20,17 +20,23 @@ public class Programa {
 			switch (option) {
 			case "1":
 				
+				break;
+			case "2":
 				
 				break;
-
+			case "3":
+				
+				break;
+			case "4":
 			default:
+				System.out.println("OPÇÃO ERRADA!");
 				break;
 			}
 			
 			
 			
 			
-		}while(!option.equals("7"));
+		}while(!option.equals("4"));
 		
 	}
 
