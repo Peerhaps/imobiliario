1d658f24c1c1a9c3fb5925ab0f3e31a0dc11ea6a
gabriel.paula <gabriel.paula@vdi-unijava-002.intranet.ciss>
2015-04-07 16:48:34 -0300

    Menu imobiliaria

--- Imobiliaria/br/com/uniciss/imobiliaria/Menu_Imobiliaria.java
+++ Imobiliaria/br/com/uniciss/imobiliaria/Menu_Imobiliaria.java
@@ -1,11 +1,35 @@
 package br.com.uniciss.imobiliaria;
 
+import java.util.Scanner;
+
 public class Menu_Imobiliaria {
 	//Menu Geral Programa
 	public static void main(String[] args) {
-		
-		
-		
+		System.out.println("BEM VINDO AO IMOBILIARIA SOFTWARE");
+		System.out.println("\n ESCOLHA AS OPÇÕES: "
+				+ "1-MENU SECRETARIO\n"
+				+ "2-MENU CORRETOR\n"
+				+ "3-MENU ADMINISTRADOR\n"
+				+ "4-SAIR");
+		//Switch com as opcoes de menu;
+		Scanner entrada = new Scanner(System.in);
+	
+		String option = entrada.nextLine();
+		do{
+			switch (option) {
+			case "1":
+				
+				
+				break;
+
+			default:
+				break;
+			}
+			
+			
+			
+			
+		}while(!option.equals("7"))
 		
 	}
 
