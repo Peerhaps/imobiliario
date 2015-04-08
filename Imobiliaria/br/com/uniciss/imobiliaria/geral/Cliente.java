2e043a0b593ef4086a4d1dd06f44062adde43c5e
guilherme.guimaraes <guilherme.guimaraes@vdi-unijava-003.intranet.ciss>
2015-04-08 16:21:14 -0300

    Exite cliente e listar cliente

--- Imobiliaria/br/com/uniciss/imobiliaria/geral/Cliente.java
+++ Imobiliaria/br/com/uniciss/imobiliaria/geral/Cliente.java
@@ -33,8 +33,8 @@ public class Cliente extends Pessoa {
 
 		while (keys.hasNext()) {
 			String key = keys.next();
-			//clientes.getJSONObject(key)
-			//lista.add((clientes.get(key)));
+			JSONObject oClientes = clientes.getJSONObject(key);
+			lista.add(oClientes.getString("Nome"));
 		}
 
 		return lista;
