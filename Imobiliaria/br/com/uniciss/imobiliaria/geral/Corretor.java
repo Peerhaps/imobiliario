aad6eb4b4810f69cbadbf021dd95966c7b97fee8
guilherme.guimaraes <guilherme.guimaraes@vdi-unijava-003.intranet.ciss>
2015-04-09 17:22:34 -0300

    Verificar se o imovel está a venda ao vender

--- Imobiliaria/br/com/uniciss/imobiliaria/geral/Corretor.java
+++ Imobiliaria/br/com/uniciss/imobiliaria/geral/Corretor.java
@@ -1,18 +1,14 @@
 package br.com.uniciss.imobiliaria.geral;
 
-
 public class Corretor extends Secretario {
-	public static void vender(ImovelVenda imovel, Cliente comprador) {
-		
-		
-		
-		imovel.excluir();
+	public static boolean vender(ImovelVenda imovel, Cliente comprador) {
+		return imovel.aVenda() && imovel.excluir();
 	}
 
 	public static void alugar(Cliente locatario, ImovelAluguel imovel) {
 
 	}
-	
+
 	@Override
 	public String getFuncao() {
 		return "corretor";
