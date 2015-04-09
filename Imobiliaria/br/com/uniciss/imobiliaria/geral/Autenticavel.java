a397ac09f700b02adf4eed005d21cd7b28c782ec
guilherme.guimaraes <guilherme.guimaraes@vdi-unijava-003.intranet.ciss>
2015-04-09 14:09:31 -0300

    Get e set senha

--- Imobiliaria/br/com/uniciss/imobiliaria/geral/Autenticavel.java
+++ Imobiliaria/br/com/uniciss/imobiliaria/geral/Autenticavel.java
@@ -1,5 +1,14 @@
 package br.com.uniciss.imobiliaria.geral;
 public abstract class Autenticavel extends Pessoa {
+	
+	public String getSenha() {
+		return this.dados.getString("Senha");
+	}
+	
+	public void setSenha(String senha) {
+		this.dados.put("Senha", senha);
+	}
+	
 	public void logar(String login, String senha) {
 	
 	}
